066bfbe61cb2a6816739dc21cc7c53b3033216f9
shiwamkumar01 <shiwampatel01@gmail.com>
2024-11-19 21:17:49 +0530

    Add files via upload

---
+++ landing.js
@@ -0,0 +1,12 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const navbar = document.getElementById('navbar');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            navbar.classList.add('scrolled');
+        } else {
+            navbar.classList.remove('scrolled');
+        }
+    });
+});
